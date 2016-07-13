f20d2f5a7da8996865a25a5f3e75dc6791e55ac6
Denis Ristic <denis.ristic@perpetuum.hr>
2016-07-13 13:06:02 +0200

    FIXED ExcelServiceProvider

--- src/denisristic/ExcelServiceProvider/Provider/ExcelServiceProvider.php
+++ src/denisristic/ExcelServiceProvider/Provider/ExcelServiceProvider.php
@@ -2,19 +2,20 @@
 
 namespace denisristic\ExcelServiceProvider\Provider;
 
+use Pimple\Container;
 use Pimple\ServiceProviderInterface;
 
 class ExcelServiceProvider implements ServiceProviderInterface
 {
     public function register(Container $app)
     {
-        $app['excel'] = $app->share(function ($app) {
+        $app['excel'] = function ($app) {
             if (isset($app['db'])) {
                 return new \denisristic\ExcelServiceProvider\Generator\ExcelDoctrine($app['db']);
             }
 
             return new \denisristic\ExcelServiceProvider\Generator\Excel();
-        });
+        };
     }
 
 }
